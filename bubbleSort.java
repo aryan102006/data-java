9b832573e052caf9453bfffc91a6b43b13ffca11
Aryan Bora <76812277+aryan102006@users.noreply.github.com>
2021-02-11 11:40:02 +0530

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,19 +1,41 @@
 import java.util.Arrays;
+import java.util.Scanner;
+public class BubbleSortUI 
+{
+    public static void main(String[] args) 
+    {
+	   int i, j;
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter the size of array : \n");
+        int n = sc.nextInt();
 
-public class bubbleSort {
-    public static void main(String[] args) {
-        int[] a = new int[] {3,2,1};
+        int array[] = new int[n]; 
+        System.out.print("Enter the elements that need to be inserted in the array : \n");
+        
+        for(i=0; i<n; i++)
+        {
+            array[i] = sc.nextInt();
+        }
 
-        for (int i = 0; i < a.length; i++) {
-            for (int j =1; j<a.length-i; j++){
-                if (a[j-1] > a[j]) {
-                    int temp = a[j];
-                    a[j] = a[j-1];
-                    a[j-1] = temp;
+        System.out.print("array before Sorting: \n"+ Arrays.toString(array));
+        System.out.print("\nSorting begins here..\n");
+
+        for(i=0; i<array.length; i++)
+        {
+            for(j=1; j<array.length-i; j++)
+            {
+                if(array[j-1] > array[j])
+                {
+                    int temp = array[j]; 
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
                 }
-
             }
         }
-        System.out.println(Arrays.toString(a));
+        System.out.print("Array after Sorting is :\n");                                 
+        for(i=0; i<n; i++)
+        {
+            System.out.print(array[i]+ " ");
+        }
     }
 }
